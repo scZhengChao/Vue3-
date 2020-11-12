2cb9794cf5f26e4a8cf02407d824109454b257cb
scZhengChao <15082810674@163.com>
2020-11-12 14:28:19 +0800

    vue3+

--- src/views/Componsition.vue
+++ src/views/Componsition.vue
@@ -9,13 +9,16 @@
         <li v-is="'p'">122</li>
         <li>{{book}}</li>
         <li>{{state.foo}}</li>
+        <li>{{foo}}</li>
+        <li :disabled='attribute'>attribute</li>
     </ul>
     <p v-highlight="'yellow'">{{ $filters.currencyUSD(accountBalance) }}</p>
+    <input type="checkbox" v-model="toggle" true-value="yes" false-value="no" />checkbox
 </div>
 </template>
 
 <script>
-import { ref, computed, onMounted,getCurrentInstance ,watchEffect,reactive} from 'vue'
+import { ref,toRefs, computed, onMounted,getCurrentInstance ,watchEffect,reactive,unref,isRef,isProxy,watch } from 'vue'
 
 export default {
     name:'Componsition',
@@ -27,7 +30,9 @@ export default {
     data(){return{
         list:[1,2,3,4,5,6,7],
         itemRefs:[],
-        accountBalance:'000'
+        accountBalance:'000',
+        attribute:0,
+        toggle:null
     }},
     methods: {
         setItemRef(el) {
@@ -63,21 +68,32 @@ export default {
         const state = reactive({
             foo: 'foo',
         })
+        const countValue = unref(state) // foo是我们要的值
+        // 等效于
+        // const foo = isRef(props.foo) ? props.foo.value : props.foo
         
         watchEffect(() => {
-            console.log(proxy.accountBalance) // foo变化会有输出
-            console.log(data.accountBalance) // foo变化会有输出
+            // console.log(proxy.accountBalance) // foo变化会有输出
+            // console.log(data.accountBalance) // foo变化会有输出
             console.log(state.foo)
+            console.log(proxy.toggle)
+        })
+        watch(_=>proxy.toggle,(newValue,oldValue)=>{
+            console.log(newValue,oldValue)
         })
         state.foo = 'foooooo' // 输出 'foooooo'
         onMounted(() =>{
             proxy.accountBalance = '8888'
+            state.foo ='mounted'
         })
         return {
             count,
             double,
             increment,
-            state
+            state,
+            watch,
+            watchEffect,
+            ...toRefs(state)
         }
     },
     mounted() {
